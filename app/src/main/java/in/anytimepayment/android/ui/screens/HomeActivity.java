21dcb7c7cb42732935996a1d71473c9dbc76c723
Charan Br <Charan.Br@in.ril.com>
2017-05-12 19:04:33 +0530

    created UI for home screen.

--- app/src/main/java/in/anytimepayment/android/ui/screens/HomeActivity.java
+++ app/src/main/java/in/anytimepayment/android/ui/screens/HomeActivity.java
@@ -2,6 +2,7 @@ package in.anytimepayment.android.ui.screens;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 import in.anytimepayment.android.R;
 
@@ -11,5 +12,11 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
     }
 }
